bdc9ed0afc3ec876ebe4d5bb335fad6ef0535337
Patrick <patricknaashat@yahoo.com>
2025-05-18 13:38:53 +0300

    add kokoro notebook

--- docker_image/fast_conformer.py
+++ docker_image/fast_conformer.py
@@ -14,10 +14,20 @@ def load_asr_model(ckpt_path, device):
         params = yaml.load(f)
     params['model'].pop('test_ds', None)
     conf = OmegaConf.create(params)
+    conf['model'].vocab_size = 32001
     
     model = nemo_asr.models.EncDecRNNTBPEModel(cfg=conf['model']).to(device)
-    model.load_state_dict(torch.load(ckpt_path, map_location=device, weights_only=False)['state_dict'])
+    incompatible_keys = [
+        'decoder.prediction.embed.weight',
+        'joint.joint_net.2.weight',
+        'joint.joint_net.2.bias'
+    ]
+    checkpoint = torch.load(ckpt_path, map_location=device, weights_only=False)['state_dict']
+
+    filtered_checkpoint = {k: v for k, v in checkpoint.items() if k not in incompatible_keys}
+    model.load_state_dict(filtered_checkpoint, strict=False)
     model.eval()
+
     return model
 
 def load_audio(audio_path):
@@ -33,32 +43,40 @@ def load_audio(audio_path):
 def infer(model, audio):
     return model.transcribe([audio])
 
+def find_audio_files(root_dir):
+    audio_files = []
+    for subdir, _, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith(".mp3") or file.endswith(".wav"):
+                audio_files.append(os.path.join(subdir, file))
+    return sorted(audio_files)
+
 def process_files(audio_files, model_path, device, model_number, results_list):
     print(f"Loading ASR model {model_number} on {device}...")
     model = load_asr_model(model_path, device)
     
     transcripts = []
     for idx, audio_file in enumerate(audio_files, start=1):
-        audio_path = os.path.join("sample_data", audio_file)
-        print(f"[{device}] Processing {idx}/{len(audio_files)}: {audio_file} using model {model_number} on {device}...")
+        filename = os.path.basename(audio_file)
+        print(f"[{device}] Processing {idx}/{len(audio_files)}: {filename} using model {model_number} on {device}...")
         
-        audio, sr = load_audio(audio_path)
+        audio, sr = load_audio(audio_file)
         if audio is None:
             continue
         
         with torch.no_grad():
             transcript = infer(model, audio)
         
-        transcripts.append([audio_file, transcript[0], device, model_number])
+        transcripts.append([filename, transcript[0].text, device, model_number])
     
     results_list.extend(transcripts)
 
 def main():
     audio_dir = "sample_data/"
-    output_csv = "t8.csv"
+    output_csv = "t.csv"
     model_path = "ASR_for_egyptian_dialect/Models/asr_model.ckpt"
     
-    audio_files = sorted([f for f in os.listdir(audio_dir) if f.endswith(".mp3")])
+    audio_files = find_audio_files(audio_dir)
     print(f"Total audio files: {len(audio_files)}")
     
     mid_index = len(audio_files) // 2
